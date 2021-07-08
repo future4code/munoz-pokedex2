86bff7f7b4ef99746acf62cda560196e34242956
Juliana Moraes <juliana.hmoraes@gmail.com>
2021-07-07 23:59:25 -0300

    layout pagina pokedexPage

--- src/pages/PokedexPage/PokedexPage.js
+++ src/pages/PokedexPage/PokedexPage.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import {} from "./Styled";
+import { Container, Topo } from './Styled.js'
+import Logo from '../../img/logo.png' 
+import { goToPokedexPage } from '../../routes/Coordinator'
 
 const PokedexPage = () => {
 
+  const history = useHistory();
+
   return (
-    <h1>Pokedex</h1>
+    <Container>
+      <Topo>
+        <button onClick={() => history.push("/")}>Lista de Pokemons</button>
+        <img src={Logo}></img>
+      </Topo>
+
+    </Container>
   );
 };
 
