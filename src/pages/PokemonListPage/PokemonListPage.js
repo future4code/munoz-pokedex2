f4c7989bf04a4b3bdc335fa8bc9c574fdc0a8f0d
Clara Meirelles <clara_meirelles@hotmail.com>
2021-07-08 20:43:16 -0300

    adicionei path no link para a tela de detalhes, assim o pokémon selecionado permanece mesmo quando atualiza a página. A lógica seguirá a mesma a partir da tela de card, que recebe o nome do pokémon como prop (fazer uma variável pro link fixo da api ou um useGetData)

--- src/pages/PokemonListPage/PokemonListPage.js
+++ src/pages/PokemonListPage/PokemonListPage.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/Header/Header";
 import CardPokemon from "../../components/CardPokemon/CardPokemon";
 import { useHistory } from "react-router-dom";
 import { goToPokedexPage } from "../../routes/Coordinator";
 import { ListContainer } from "./Styled";
+import axios from "axios";
+
 
 const PokemonListPage = () => {
   const history = useHistory();
+  const [pokemonList, setPokemonList] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=20`)
+      .then((response) => {
+        setPokemonList(response.data.results)
+      })
+      .catch((err) => {
+        console.log('erro', err)
+      })
+  }, [])
 
   return (
     <>
@@ -15,10 +28,15 @@ const PokemonListPage = () => {
         ControllerButtonMain={() => goToPokedexPage(history)}
       />
       <ListContainer>
+        {pokemonList.length>0 &&
+        pokemonList.map((pokemon)=> {
+          return <CardPokemon name={pokemon.name} key={pokemon.url}/>
+        })
+        }
+{/*         
         <CardPokemon />
         <CardPokemon />
-        <CardPokemon />
-        <CardPokemon />
+        <CardPokemon /> */}
       </ListContainer>
     </>
   );
