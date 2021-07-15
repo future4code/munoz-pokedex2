a64aa9c25746d47be59b50eda9831debb2526bac
Juliana Moraes <juliana.hmoraes@gmail.com>
2021-07-14 21:41:49 -0300

    alteracao cards

--- src/components/CardPokemon/Styled.js
+++ src/components/CardPokemon/Styled.js
@@ -1,21 +1,31 @@
 import styled from "styled-components";
 
 export const CardContainerMain = styled.div`
-  height: 40vh;
+  height: 35vh;
   width: 300px;
   margin: 40px;
   display: grid;
   grid-template-rows: 85% 15%;
+
+  button {
+    background-color: black;
+    color: white;
+    border: 1px solid gray;
+  }
 `;
 
 export const ImageContainer = styled.div`
   background: black;
+  opacity: 2px;
   display: flex;
   justify-content: center;
   align-items: center;
+
 `;
 
-export const ImagePokemon = styled.img``;
+export const ImagePokemon = styled.img`
+  height: 20vh;
+`;
 
 export const ContainerButton = styled.div`
   display: grid;
